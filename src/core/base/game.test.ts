77a984066a3cf07e9778aec8fbb9315ea9dc9e3b
Bruno Boi (boi) <boi@odoo.com>
2024-11-09 22:16:05 +0100

    feat: add initial project structure

---
+++ src/core/base/game.test.ts
@@ -0,0 +1,3 @@
+/**
+ * The Game is the main class that defines the behaviour of the game (and maybe it's layout).
+*/
---
+++ src/core/base/zone.test.ts
@@ -0,0 +1,10 @@
+/**
+ * Zones are regions or spaces of the Board.
+ * They are either playable or not.
+ * A zone could be for instance:
+ * - a player's hand
+ * - a drawing pile
+ * - a discard pile
+ * - the center area of the board
+ * - etc
+ */
